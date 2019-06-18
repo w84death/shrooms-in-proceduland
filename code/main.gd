ec049295f3c1b51e7f781c80f14d8c6454bd4c26
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2019-06-18 20:29:22 +0200

    nice dust + awesome clouds

--- code/main.gd
+++ code/main.gd
@@ -1,5 +1,13 @@
 extends Spatial
 
+# P1X
+# Directed by Krzysztof Jankowski
+# Music by Mirek Harenda
+# SoundFont Reader by Yui Kinomoto @arlez80
+#
+# Copyright(2019) P1X
+# https://p1x.in
+
 export var next_scene_bigfile = "main"
 
 func _input(event):
